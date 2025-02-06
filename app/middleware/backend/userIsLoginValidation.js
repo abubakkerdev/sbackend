f02121f7484ac8c44775b33d2f3f3e5d1c92834c
Ababukker Siddik <96437368+abubakkerdev@users.noreply.github.com>
2025-02-06 14:09:34 +0600

    project upload

---
+++ app/middleware/backend/userIsLoginValidation.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+
+const userIsLoginValidation = (req, res, next) => {
+  jwt.verify(
+    JSON.parse(req.signedCookies.userAllInfo).tokens,
+    process.env.LOGIN_SECRET_KEY,
+    function (err, decoded) {
+      if (decoded) {
+        next();
+      } else {
+        return res.send({
+          error: "Request URL not found on this Server.",
+        });
+      }
+    }
+  );
+};
+
+module.exports = userIsLoginValidation;
+ 
